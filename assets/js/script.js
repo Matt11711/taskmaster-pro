1e5d083f46797d2ff859672d214faea915597109
Matt11711 <matthew.d.allen97@gmail.com>
2022-03-05 21:30:38 -0500

    finished touchups and custom styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -147,6 +147,10 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
+  activate: function() {
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
+  },
   update: function (event) {
     // array to store the task data in
     var tempArr = [];
@@ -172,6 +176,18 @@ $(".card .list-group").sortable({
     tasks[arrName] = tempArr;
     saveTasks();
   },
+   deactivate: function() {
+$(this).removeClass("dropover")
+$(".bottom-trash").removeClass("bottom-trash-drag")
+   },
+   over: function(event) {
+     $(event.target).addClass("dropover-active")
+    
+   },
+   out: function(event) {
+    $(event.target).removeClass("dropover-active")
+   
+  }
 });
 
 $("#trash").droppable({
@@ -181,10 +197,10 @@ $("#trash").droppable({
     ui.draggable.remove();
   },
   over: function (event, ui) {
-    console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function (event, ui) {
-    console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active")
   },
 });
 
@@ -205,7 +221,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -234,6 +250,12 @@ $("#remove-tasks").on("click", function () {
   }
   saveTasks();
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 // load tasks for the first time
 loadTasks();
